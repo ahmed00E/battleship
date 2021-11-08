24eeca8c8504e89544d1628334a148bade86c7ac
ahmed00E <84589474+ahmed00E@users.noreply.github.com>
2021-11-08 17:27:01 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from './components/Button';
+
+
+ 
+
 
 class App  extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      activeTab: "Add",
+      items: []
+    }
+
+
+    
+     
+
+  }
+
+  
+
   render() { 
     return (
     <div>
       <h1>salut</h1>
+      <Button />
     </div>
     )
   }
---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Button extends Component {
+
+    
+  render() {
+    
+
+    return (
+         
+        <button type="submit"
+         className=
+         "btn btn-primary btn-block">START
+                               
+        </button>
+         
+    )
+  }
+}
+
+export default Button
