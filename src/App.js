62962c6d44fda39b9ff041cf56f996c6eaabedda
ahmed00E <84589474+ahmed00E@users.noreply.github.com>
2021-11-09 10:33:31 +0100

    add button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Button from "./components/Button"
 class App  extends React.Component {
   render() { 
     return (
     <div>
       <h1>salut</h1>
+      <Button/> 
     </div>
     )
   }
---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Button extends Component {
+
+    
+  render() {
+    
+
+    return (
+         
+        <button type="button" className="btn btn-primary">START</button>
+         
+    )
+  }
+}
+
+export default Button
